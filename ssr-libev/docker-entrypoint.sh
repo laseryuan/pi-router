bf78ca5ec671a30b79a6dd232e426d51f435afde
Laser Yuan <laser.yuan@gmail.com>
2018-11-28 15:30:05 +0800

    Display router ip in help message

--- ssr-libev/docker-entrypoint.sh
+++ ssr-libev/docker-entrypoint.sh
@@ -20,13 +20,16 @@ run-router() {
 }
 
 run-help() {
+  echo "* Step 1: Router IP (eth0):"
+  echo `ip address show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+  echo "* Step 2: Run following command to start the router:"
   echo docker run -d --restart always \
     --sysctl net.ipv4.conf.all.route_localnet=1 \
     --net=host \
     --cap-add=NET_ADMIN \
     --cap-add=NET_RAW \
     --name pi-router \
-    -e TPROXY_PORT={19040} \
+    -e TPROXY_PORT=19040 \
     -e SERVER_NAME={changeme.com} \
     -e SERVER_PORT={14443} \
     -e SERVER_PASSWORD={MY_SSPASSWORD} \
